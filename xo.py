6e96b12bd3171c74c7945a80c56ae4b16e1d9e14
aminebenichou <aamine060@gmail.com>
2026-07-08 17:56:55 +0100

    sync

--- xo.py
+++ xo.py
@@ -20,6 +20,27 @@ cells = [
     {'start_pos':(200,400), 'x_checked':False, 'o_checked':False, 'end_pos':(390,590)},
     {'start_pos':(400,400), 'x_checked':False, 'o_checked':False, 'end_pos':(590,590)},
 ]
+combinations=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+
+    [0,4,8],
+    [2,4,6],
+
+]
+
+def get_winner(combinations):
+    for combo in combinations:
+        if cells[combo[0]][f'{player}_checked'] and cells[combo[1]][f'{player}_checked'] and cells[combo[2]][f'{player}_checked']:
+            print("we have a winner")
+            break   
+                
+
 pos = (0,0)
 player = "x"
 while running:
@@ -39,13 +60,18 @@ while running:
     for cell in cells:
         pygame.draw.rect(screen, "white", (cell['start_pos'][0],cell['start_pos'][1],190,190))
 
-        if  cell['start_pos'][0]<pos[0]<cell['end_pos'][0] and cell['start_pos'][1]<pos[1]<cell['end_pos'][1]:
+        if  cell['start_pos'][0]<pos[0]<cell['end_pos'][0] and cell['start_pos'][1]<pos[1]<cell['end_pos'][1] and  (not cell['o_checked'] and not cell['x_checked']):
+            
             cell[f'{player}_checked']=True
-            pos=(0,0)
-            if player == "x":
+            get_winner(combinations)
+            if player =="x":
                 player="o"
             else:
                 player="x"
+
+
+            pos=(0,0)
+            
 
         if cell['o_checked']:
             pygame.draw.circle(screen,'red',(cell['start_pos'][0]+95,cell['start_pos'][1]+95), 60)
@@ -53,6 +79,9 @@ while running:
         if cell['x_checked']:
             pygame.draw.line(screen, 'blue', cell['start_pos'], cell['end_pos'], 10)
             pygame.draw.line(screen, 'blue', (cell['end_pos'][0],cell['start_pos'][1]), (cell['start_pos'][0],cell['end_pos'][1]), 10)
+    
+    
+    
     # flip() the display to put your work on screen
     pygame.display.flip()
 
